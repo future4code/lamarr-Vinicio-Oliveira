86eb738721be3f819e6103087f03f6a115218440
Vinicio Cassimiro Oliveira <vinicio.cassimiro@gmail.com>
2022-05-05 22:39:23 -0300

    Atualizando funcoes-de-array

---
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -0,0 +1,28 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+console.log("--- Seja bem vindo(a) ao jogo de Blackjack! ---")
+
+if(confirm("Deseja iniciar uma nova rodada?")){
+
+} else {
+   console.log("O jogo acabou!")
+}
+
+
+
+
+
+
+
+
