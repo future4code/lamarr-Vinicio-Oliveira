2539f0f19ec6001423b551368b61614da869a2e5
Vinicio Cassimiro Oliveira <vinicio.cassimiro@gmail.com>
2022-07-07 22:07:13 -0300

    atualização insta4

--- modulo3/estados-insta4/insta4/src/style.js
+++ modulo3/estados-insta4/insta4/src/style.js
@@ -6,3 +6,15 @@ export const MainContainer = styled.div`
     flex-direction: column;
     align-items: center
 `
+
+export const Formulario = styled.form`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+label {
+    padding-top: 15px;
+}
+
+`
