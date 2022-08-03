7f9b8797690215a688adbeb01b1fb232e9c63728
Vinicio Cassimiro Oliveira <vinicio.cassimiro@gmail.com>
2022-08-03 19:25:17 -0300

    astromatch

---
+++ modulo3/astromatch/astromatch/src/App.js
@@ -0,0 +1,14 @@
+import { GlobalStyle } from "./GlobalStyledComponents";
+
+function App() {
+  return (
+    <div>
+      <GlobalStyle/>
+      <h1>astromatch</h1>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ modulo3/astromatch/astromatch/src/GlobalStyledComponents.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    h1 {
+        font-family: 'Baloo Tamma 2', cursive;
+    }
+
+    body {
+        background-color: lightgray;
+    }
+
+    *{
+        margin: 0;
+        padding: 0;
+    }
+`
