9360c2994ac499dcd09596bc24a8a6f4a8803aeb
Vinicio Cassimiro Oliveira <vinicio.cassimiro@gmail.com>
2022-10-06 20:42:22 -0300

    Realizando exercicio 2

--- modulo.5/revisao-javascript-backend/backend/exercicio2/index.js
+++ modulo.5/revisao-javascript-backend/backend/exercicio2/index.js
@@ -1,1 +1,43 @@
 console.log("exercício 2");
+
+let clientes = [
+    { id: 1, nome: "Fulano" },
+    { id: 2, nome: "Ciclano" },
+    { id: 3, nome: "Beltrano" },
+    { id: 4, nome: "Fulana" }
+]
+
+
+const add = (id, nome) => {
+
+    let i
+
+    clientes.push({
+        id: id, nome: nome
+    })
+
+    for (i = 0; i < clientes.length; i++)
+        if (id === clientes[i].id) {
+            return "Erro"
+        } else {
+            return clientes
+        }
+
+}
+
+// console.log(add(1, "Cachorro"))
+
+let result
+
+const tabuada = (param) => {
+    for (let i = 0; i <= 10; i++) {
+        if (param === '' || param < 10) {
+            result = param * i
+            console.log(`${param} x ${i} = ${result}`)
+        } else {
+            console.log( 'erro')
+        }
+    }
+}
+
+tabuada('15')
