8f659e1578e6c2aec1c7d2382158dc8803b1d216
Vinicio Cassimiro Oliveira <vinicio.cassimiro@gmail.com>
2022-10-17 19:07:20 -0300

    finalizando exercicio 2

--- modulo.5/intro-typescript/src/exercicio2.ts
+++ modulo.5/intro-typescript/src/exercicio2.ts
@@ -1,9 +1,9 @@
 const userInput = require("readline-sync")
 
 function imprimeTresCoresFavoritas() {
-    const cor1 = userInput.question.prompt("Insira sua primeira cor favorita")
-    const cor2 = userInput.question.prompt("Insira sua segunda cor favorita")
-    const cor3 = userInput.question.prompt("Insira sua terceira cor favorita")
+    const cor1 = userInput.question("Insira sua primeira cor favorita: ")
+    const cor2 = userInput.question("Insira sua segunda cor favorita: ")
+    const cor3 = userInput.question("Insira sua terceira cor favorita: ")
     console.log([cor1, cor2, cor3])
  }
 
