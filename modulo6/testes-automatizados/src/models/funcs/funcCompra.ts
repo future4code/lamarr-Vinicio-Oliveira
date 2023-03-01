30280d0408b41f2be1d61179468b3bcb536d68f2
Vinicio Cassimiro Oliveira <vinicio.cassimiro@gmail.com>
2023-02-28 22:48:46 -0300

    Realizando exercícios de testes

---
+++ modulo6/testes-automatizados/src/models/funcs/funcCompra.ts
@@ -0,0 +1,9 @@
+function realizaCompra(user: User, value: number): User | undefined {
+	if(user.balance >= value) {
+		return {
+			...user,
+			balance: user.balance - value		
+		}
+	}
+	return undefined
+}
